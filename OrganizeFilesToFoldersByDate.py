56362bd934ec3469aab9a59c4543bc6ccb244b34
coolatom <artemkucheriaviy@gmail.com>
2024-07-03 16:33:17 +0300

    Translated UA-EN OrganizeFilesToFoldersByDate.py

--- OrganizeFilesToFoldersByDate.py
+++ OrganizeFilesToFoldersByDate.py
@@ -4,10 +4,10 @@ import shutil
 from pathlib import Path
 
 def organize_files_by_date(directory):
-    # Регулярний вираз для пошуку дати у форматі `рік-місяць-день`
+    # A regular expression for searching for filenames containing a Date in `year-month-day' format
     date_pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
 
-    # Підбір файлів у папці
+    # View files in a Directory
     for filename in os.listdir(directory):
         match = date_pattern.search(filename)
         if match:
